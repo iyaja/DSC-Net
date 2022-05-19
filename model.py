9b96645f1ef89de000f5d1e70390a35942bebe44
Ajay Uppili Arasanipalai <ajayuppili@gmail.com>
2022-05-18 23:57:25 -0500

    refactor: linear variable names

--- model.py
+++ model.py
@@ -12,162 +12,33 @@ import scipy.io as sio
 import math
 
 
-class Conv2dSamePad(nn.Module):
-    """
-    Implement Tensorflow's 'SAME' padding mode in Conv2d.
-    When an odd number, say `m`, of pixels are need to pad, Tensorflow will pad one more column at right or one more
-    row at bottom. But Pytorch will pad `m+1` pixels, i.e., Pytorch always pads in both sides.
-    So we can pad the tensor in the way of Tensorflow before call the Conv2d module.
-    """
-
-    def __init__(self, kernel_size, stride):
-        super(Conv2dSamePad, self).__init__()
-        self.kernel_size = (
-            kernel_size
-            if type(kernel_size) in [list, tuple]
-            else [kernel_size, kernel_size]
-        )
-        self.stride = stride if type(stride) in [list, tuple] else [stride, stride]
-
-    def forward(self, x):
-        in_height = x.size(2)
-        in_width = x.size(3)
-        out_height = math.ceil(float(in_height) / float(self.stride[0]))
-        out_width = math.ceil(float(in_width) / float(self.stride[1]))
-        pad_along_height = (
-            (out_height - 1) * self.stride[0] + self.kernel_size[0] - in_height
-        )
-        pad_along_width = (
-            (out_width - 1) * self.stride[1] + self.kernel_size[1] - in_width
-        )
-        pad_top = math.floor(pad_along_height / 2)
-        pad_left = math.floor(pad_along_width / 2)
-        pad_bottom = pad_along_height - pad_top
-        pad_right = pad_along_width - pad_left
-        return F.pad(x, [pad_left, pad_right, pad_top, pad_bottom], "constant", 0)
-
-
-class ConvTranspose2dSamePad(nn.Module):
-    """
-    This module implements the "SAME" padding mode for ConvTranspose2d as in Tensorflow.
-    A tensor with width w_in, feed it to ConvTranspose2d(ci, co, kernel, stride), the width of output tensor T_nopad:
-        w_nopad = (w_in - 1) * stride + kernel
-    If we use padding, i.e., ConvTranspose2d(ci, co, kernel, stride, padding, output_padding), the width of T_pad:
-        w_pad = (w_in - 1) * stride + kernel - (2*padding - output_padding) = w_nopad - (2*padding - output_padding)
-    Yes, in ConvTranspose2d, more padding, the resulting tensor is smaller, i.e., the padding is actually deleting row/col.
-    If `pad`=(2*padding - output_padding) is odd, Pytorch deletes more columns in the left, i.e., the first ceil(pad/2) and
-    last `pad - ceil(pad/2)` columns of T_nopad are deleted to get T_pad.
-    In contrast, Tensorflow deletes more columns in the right, i.e., the first floor(pad/2) and last `pad - floor(pad/2)`
-    columns are deleted.
-    For the height, Pytorch deletes more rows at top, while Tensorflow at bottom.
-    In practice, we usually want `w_pad = w_in * stride`, i.e., the "SAME" padding mode in Tensorflow,
-    so the number of columns to delete:
-        pad = 2*padding - output_padding = kernel - stride
-    We can solve the above equation and get:
-        padding = ceil((kernel - stride)/2), and
-        output_padding = 2*padding - (kernel - stride) which is either 1 or 0.
-    But to get the same result with Tensorflow, we should delete values by ourselves instead of using padding and
-    output_padding in ConvTranspose2d.
-    To get there, we check the following conditions:
-    If pad = kernel - stride is even, we can directly set padding=pad/2 and output_padding=0 in ConvTranspose2d.
-    If pad = kernel - stride is odd, we can use ConvTranspose2d to get T_nopad, and then delete `pad` rows/columns by
-    ourselves; or we can use ConvTranspose2d to delete `pad - 1` by setting `padding=(pad - 1) / 2` and `ouput_padding=0`
-    and then delete the last row/column of the resulting tensor by ourselves.
-    Here we implement the former case.
-    This module should be called after the ConvTranspose2d module with shared kernel_size and stride values.
-    And this module can only output a tensor with shape `stride * size_input`.
-    A more flexible module can be found in `yaleb.py` which can output arbitrary size as specified.
-    """
-
-    def __init__(self, kernel_size, stride):
-        super(ConvTranspose2dSamePad, self).__init__()
-        self.kernel_size = (
-            kernel_size
-            if type(kernel_size) in [list, tuple]
-            else [kernel_size, kernel_size]
-        )
-        self.stride = stride if type(stride) in [list, tuple] else [stride, stride]
-
-    def forward(self, x):
-        in_height = x.size(2)
-        in_width = x.size(3)
-        pad_height = self.kernel_size[0] - self.stride[0]
-        pad_width = self.kernel_size[1] - self.stride[1]
-        pad_top = pad_height // 2
-        pad_bottom = pad_height - pad_top
-        pad_left = pad_width // 2
-        pad_right = pad_width - pad_left
-        return x[
-            :, :, pad_top : in_height - pad_bottom, pad_left : in_width - pad_right
-        ]
-
-
-class ConvAE(nn.Module):
-    def __init__(self, channels, kernels):
+class AutoEncoder(nn.Module):
+    def __init__(self, sequence_length, hidden_sizes):
         """
         :param channels: a list containing all channels including the input image channel (1 for gray, 3 for RGB)
-        :param kernels:  a list containing all kernel sizes, it should satisfy: len(kernels) = len(channels) - 1.
         """
-        super(ConvAE, self).__init__()
-        assert isinstance(channels, list) and isinstance(kernels, list)
+        super(AutoEncoder, self).__init__()
+        assert isinstance(hidden_sizes, list)
         self.encoder = nn.Sequential()
-        for i in range(1, len(channels)):
-            #  Each layer will divide the size of feature map by 2
-            self.encoder.add_module("pad%d" % i, Conv2dSamePad(kernels[i - 1], 2))
-            self.encoder.add_module(
-                "conv%d" % i,
-                nn.Conv2d(
-                    channels[i - 1], channels[i], kernel_size=kernels[i - 1], stride=2
-                ),
-            )
-            self.encoder.add_module("relu%d" % i, nn.ReLU(True))
-
-        self.decoder = nn.Sequential()
-        channels = list(reversed(channels))
-        kernels = list(reversed(kernels))
-        for i in range(len(channels) - 1):
-            # Each layer will double the size of feature map
-            self.decoder.add_module(
-                "deconv%d" % (i + 1),
-                nn.ConvTranspose2d(
-                    channels[i], channels[i + 1], kernel_size=kernels[i], stride=2
-                ),
-            )
-            self.decoder.add_module("padd%d" % i, ConvTranspose2dSamePad(kernels[i], 2))
-            self.decoder.add_module("relud%d" % i, nn.ReLU(True))
-
-    def forward(self, x):
-        h = self.encoder(x)
-        y = self.decoder(h)
-        return y
-
-
-class LinearAE(nn.Module):
-    def __init__(self, channels, kernels):
-        """
-        :param channels: a list containing all channels including the input image channel (1 for gray, 3 for RGB)
-        :param kernels:  a list containing all kernel sizes, it should satisfy: len(kernels) = len(channels) - 1.
-        """
-        super(LinearAE, self).__init__()
-        assert isinstance(channels, list) and isinstance(kernels, list)
-        self.encoder = nn.Sequential()
-        for i in range(1, len(channels)):
+        self.encoder.add_module("input", nn.Linear(sequence_length, hidden_sizes[0]))
+        for i in range(1, len(hidden_sizes)):
             #  Each layer will divide the size of feature map by 2
             self.encoder.add_module(
                 "linear%d" % i,
-                nn.Linear(channels[i - 1], channels[i]),
+                nn.Linear(hidden_sizes[i - 1], hidden_sizes[i]),
             )
             self.encoder.add_module("relu%d" % i, nn.ReLU(True))
 
         self.decoder = nn.Sequential()
-        channels = list(reversed(channels))
-        kernels = list(reversed(kernels))
-        for i in range(len(channels) - 1):
+        hidden_sizes = list(reversed(hidden_sizes))
+        for i in range(len(hidden_sizes) - 1):
             # Each layer will double the size of feature map
             self.decoder.add_module(
-                "inv-linear%d" % (i + 1), nn.Linear(channels[i], channels[i + 1])
+                "inv-linear%d" % (i + 1),
+                nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]),
             )
             self.decoder.add_module("relud%d" % i, nn.ReLU(True))
+        self.decoder.add_module("output", nn.Linear(hidden_sizes[-1], sequence_length))
 
     def forward(self, x):
         h = self.encoder(x)
@@ -188,10 +59,10 @@ class SelfExpression(nn.Module):
 
 
 class DSCNet(nn.Module):
-    def __init__(self, channels, kernels, num_sample):
+    def __init__(self, num_samples, sequence_length, hidden_sizes):
         super(DSCNet, self).__init__()
-        self.n = num_sample
-        self.ae = LinearAE(channels, kernels)
+        self.n = num_samples
+        self.ae = AutoEncoder(sequence_length, hidden_sizes)
         self.self_expression = SelfExpression(self.n)
 
     def forward(self, x):  # shape=[n, c, w, h]
@@ -337,7 +208,7 @@ if __name__ == "__main__":
         dim_subspace = 3  # dimension of each subspace
         ro = 1  #
 
-    dscnet = DSCNet(num_sample=num_sample, channels=channels, kernels=kernels)
+    dscnet = DSCNet(num_samples=num_sample, channels=channels, kernels=kernels)
     dscnet.to(device)
 
     # load the pretrained weights which are provided by the original author in

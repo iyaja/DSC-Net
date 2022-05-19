0c3367d949818bab674523ccede295e8ed47b06b
Ajay Uppili Arasanipalai <ajayuppili@gmail.com>
2022-05-18 23:22:33 -0500

    chore: push

--- model.py
+++ model.py
@@ -22,7 +22,11 @@ class Conv2dSamePad(nn.Module):
 
     def __init__(self, kernel_size, stride):
         super(Conv2dSamePad, self).__init__()
-        self.kernel_size = kernel_size if type(kernel_size) in [list, tuple] else [kernel_size, kernel_size]
+        self.kernel_size = (
+            kernel_size
+            if type(kernel_size) in [list, tuple]
+            else [kernel_size, kernel_size]
+        )
         self.stride = stride if type(stride) in [list, tuple] else [stride, stride]
 
     def forward(self, x):
@@ -30,13 +34,17 @@ class Conv2dSamePad(nn.Module):
         in_width = x.size(3)
         out_height = math.ceil(float(in_height) / float(self.stride[0]))
         out_width = math.ceil(float(in_width) / float(self.stride[1]))
-        pad_along_height = ((out_height - 1) * self.stride[0] + self.kernel_size[0] - in_height)
-        pad_along_width = ((out_width - 1) * self.stride[1] + self.kernel_size[1] - in_width)
+        pad_along_height = (
+            (out_height - 1) * self.stride[0] + self.kernel_size[0] - in_height
+        )
+        pad_along_width = (
+            (out_width - 1) * self.stride[1] + self.kernel_size[1] - in_width
+        )
         pad_top = math.floor(pad_along_height / 2)
         pad_left = math.floor(pad_along_width / 2)
         pad_bottom = pad_along_height - pad_top
         pad_right = pad_along_width - pad_left
-        return F.pad(x, [pad_left, pad_right, pad_top, pad_bottom], 'constant', 0)
+        return F.pad(x, [pad_left, pad_right, pad_top, pad_bottom], "constant", 0)
 
 
 class ConvTranspose2dSamePad(nn.Module):
@@ -73,7 +81,11 @@ class ConvTranspose2dSamePad(nn.Module):
 
     def __init__(self, kernel_size, stride):
         super(ConvTranspose2dSamePad, self).__init__()
-        self.kernel_size = kernel_size if type(kernel_size) in [list, tuple] else [kernel_size, kernel_size]
+        self.kernel_size = (
+            kernel_size
+            if type(kernel_size) in [list, tuple]
+            else [kernel_size, kernel_size]
+        )
         self.stride = stride if type(stride) in [list, tuple] else [stride, stride]
 
     def forward(self, x):
@@ -85,7 +97,9 @@ class ConvTranspose2dSamePad(nn.Module):
         pad_bottom = pad_height - pad_top
         pad_left = pad_width // 2
         pad_right = pad_width - pad_left
-        return x[:, :, pad_top:in_height - pad_bottom, pad_left: in_width - pad_right]
+        return x[
+            :, :, pad_top : in_height - pad_bottom, pad_left : in_width - pad_right
+        ]
 
 
 class ConvAE(nn.Module):
@@ -99,20 +113,61 @@ class ConvAE(nn.Module):
         self.encoder = nn.Sequential()
         for i in range(1, len(channels)):
             #  Each layer will divide the size of feature map by 2
-            self.encoder.add_module('pad%d' % i, Conv2dSamePad(kernels[i - 1], 2))
-            self.encoder.add_module('conv%d' % i,
-                                    nn.Conv2d(channels[i - 1], channels[i], kernel_size=kernels[i - 1], stride=2))
-            self.encoder.add_module('relu%d' % i, nn.ReLU(True))
+            self.encoder.add_module("pad%d" % i, Conv2dSamePad(kernels[i - 1], 2))
+            self.encoder.add_module(
+                "conv%d" % i,
+                nn.Conv2d(
+                    channels[i - 1], channels[i], kernel_size=kernels[i - 1], stride=2
+                ),
+            )
+            self.encoder.add_module("relu%d" % i, nn.ReLU(True))
 
         self.decoder = nn.Sequential()
         channels = list(reversed(channels))
         kernels = list(reversed(kernels))
         for i in range(len(channels) - 1):
             # Each layer will double the size of feature map
-            self.decoder.add_module('deconv%d' % (i + 1),
-                                    nn.ConvTranspose2d(channels[i], channels[i + 1], kernel_size=kernels[i], stride=2))
-            self.decoder.add_module('padd%d' % i, ConvTranspose2dSamePad(kernels[i], 2))
-            self.decoder.add_module('relud%d' % i, nn.ReLU(True))
+            self.decoder.add_module(
+                "deconv%d" % (i + 1),
+                nn.ConvTranspose2d(
+                    channels[i], channels[i + 1], kernel_size=kernels[i], stride=2
+                ),
+            )
+            self.decoder.add_module("padd%d" % i, ConvTranspose2dSamePad(kernels[i], 2))
+            self.decoder.add_module("relud%d" % i, nn.ReLU(True))
+
+    def forward(self, x):
+        h = self.encoder(x)
+        y = self.decoder(h)
+        return y
+
+
+class LinearAE(nn.Module):
+    def __init__(self, channels, kernels):
+        """
+        :param channels: a list containing all channels including the input image channel (1 for gray, 3 for RGB)
+        :param kernels:  a list containing all kernel sizes, it should satisfy: len(kernels) = len(channels) - 1.
+        """
+        super(LinearAE, self).__init__()
+        assert isinstance(channels, list) and isinstance(kernels, list)
+        self.encoder = nn.Sequential()
+        for i in range(1, len(channels)):
+            #  Each layer will divide the size of feature map by 2
+            self.encoder.add_module(
+                "linear%d" % i,
+                nn.Linear(channels[i - 1], channels[i]),
+            )
+            self.encoder.add_module("relu%d" % i, nn.ReLU(True))
+
+        self.decoder = nn.Sequential()
+        channels = list(reversed(channels))
+        kernels = list(reversed(kernels))
+        for i in range(len(channels) - 1):
+            # Each layer will double the size of feature map
+            self.decoder.add_module(
+                "inv-linear%d" % (i + 1), nn.Linear(channels[i], channels[i + 1])
+            )
+            self.decoder.add_module("relud%d" % i, nn.ReLU(True))
 
     def forward(self, x):
         h = self.encoder(x)
@@ -123,7 +178,9 @@ class ConvAE(nn.Module):
 class SelfExpression(nn.Module):
     def __init__(self, n):
         super(SelfExpression, self).__init__()
-        self.Coefficient = nn.Parameter(1.0e-8 * torch.ones(n, n, dtype=torch.float32), requires_grad=True)
+        self.Coefficient = nn.Parameter(
+            1.0e-8 * torch.ones(n, n, dtype=torch.float32), requires_grad=True
+        )
 
     def forward(self, x):  # shape=[n, d]
         y = torch.matmul(self.Coefficient, x)
@@ -134,7 +191,7 @@ class DSCNet(nn.Module):
     def __init__(self, channels, kernels, num_sample):
         super(DSCNet, self).__init__()
         self.n = num_sample
-        self.ae = ConvAE(channels, kernels)
+        self.ae = LinearAE(channels, kernels)
         self.self_expression = SelfExpression(self.n)
 
     def forward(self, x):  # shape=[n, c, w, h]
@@ -150,47 +207,70 @@ class DSCNet(nn.Module):
         return x_recon, z, z_recon
 
     def loss_fn(self, x, x_recon, z, z_recon, weight_coef, weight_selfExp):
-        loss_ae = F.mse_loss(x_recon, x, reduction='sum')
+        loss_ae = F.mse_loss(x_recon, x, reduction="sum")
         loss_coef = torch.sum(torch.pow(self.self_expression.Coefficient, 2))
-        loss_selfExp = F.mse_loss(z_recon, z, reduction='sum')
+        loss_selfExp = F.mse_loss(z_recon, z, reduction="sum")
         loss = loss_ae + weight_coef * loss_coef + weight_selfExp * loss_selfExp
 
         return loss
 
 
-def train(model,  # type: DSCNet
-          x, y, epochs, lr=1e-3, weight_coef=1.0, weight_selfExp=150, device='cuda',
-          alpha=0.04, dim_subspace=12, ro=8, show=10):
+def train(
+    model,  # type: DSCNet
+    x,
+    y,
+    epochs,
+    lr=1e-3,
+    weight_coef=1.0,
+    weight_selfExp=150,
+    device="cuda",
+    alpha=0.04,
+    dim_subspace=12,
+    ro=8,
+    show=10,
+):
     optimizer = optim.Adam(model.parameters(), lr=lr)
     if not isinstance(x, torch.Tensor):
         x = torch.tensor(x, dtype=torch.float32, device=device)
     x = x.to(device)
     if isinstance(y, torch.Tensor):
-        y = y.to('cpu').numpy()
+        y = y.to("cpu").numpy()
     K = len(np.unique(y))
     for epoch in range(epochs):
         x_recon, z, z_recon = model(x)
-        loss = model.loss_fn(x, x_recon, z, z_recon, weight_coef=weight_coef, weight_selfExp=weight_selfExp)
+        loss = model.loss_fn(
+            x,
+            x_recon,
+            z,
+            z_recon,
+            weight_coef=weight_coef,
+            weight_selfExp=weight_selfExp,
+        )
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         if epoch % show == 0 or epoch == epochs - 1:
-            C = model.self_expression.Coefficient.detach().to('cpu').numpy()
+            C = model.self_expression.Coefficient.detach().to("cpu").numpy()
             y_pred = spectral_clustering(C, K, dim_subspace, alpha, ro)
-            print('Epoch %02d: loss=%.4f, acc=%.4f, nmi=%.4f' %
-                  (epoch, loss.item() / y_pred.shape[0], acc(y, y_pred), nmi(y, y_pred)))
+            print(
+                "Epoch %02d: loss=%.4f, acc=%.4f, nmi=%.4f"
+                % (epoch, loss.item() / y_pred.shape[0], acc(y, y_pred), nmi(y, y_pred))
+            )
 
 
 if __name__ == "__main__":
     import argparse
     import warnings
 
-    parser = argparse.ArgumentParser(description='DSCNet')
-    parser.add_argument('--db', default='coil20',
-                        choices=['coil20', 'coil100', 'orl', 'reuters10k', 'stl'])
-    parser.add_argument('--show-freq', default=10, type=int)
-    parser.add_argument('--ae-weights', default=None)
-    parser.add_argument('--save-dir', default='results')
+    parser = argparse.ArgumentParser(description="DSCNet")
+    parser.add_argument(
+        "--db",
+        default="coil20",
+        choices=["coil20", "coil100", "orl", "reuters10k", "stl"],
+    )
+    parser.add_argument("--show-freq", default=10, type=int)
+    parser.add_argument("--ae-weights", default=None)
+    parser.add_argument("--save-dir", default="results")
     args = parser.parse_args()
     print(args)
     import os
@@ -198,12 +278,12 @@ if __name__ == "__main__":
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     db = args.db
-    if db == 'coil20':
+    if db == "coil20":
         # load data
-        data = sio.loadmat('datasets/COIL20.mat')
-        x, y = data['fea'].reshape((-1, 1, 32, 32)), data['gnd']
+        data = sio.loadmat("datasets/COIL20.mat")
+        x, y = data["fea"].reshape((-1, 1, 32, 32)), data["gnd"]
         y = np.squeeze(y - 1)  # y in [0, 1, ..., K-1]
 
         # network and optimization parameters
@@ -218,11 +298,13 @@ if __name__ == "__main__":
         alpha = 0.04  # threshold of C
         dim_subspace = 12  # dimension of each subspace
         ro = 8  #
-        warnings.warn("You can uncomment line#64 in post_clustering.py to get better result for this dataset!")
-    elif db == 'coil100':
+        warnings.warn(
+            "You can uncomment line#64 in post_clustering.py to get better result for this dataset!"
+        )
+    elif db == "coil100":
         # load data
-        data = sio.loadmat('datasets/COIL100.mat')
-        x, y = data['fea'].reshape((-1, 1, 32, 32)), data['gnd']
+        data = sio.loadmat("datasets/COIL100.mat")
+        x, y = data["fea"].reshape((-1, 1, 32, 32)), data["gnd"]
         y = np.squeeze(y - 1)  # y in [0, 1, ..., K-1]
 
         # network and optimization parameters
@@ -237,10 +319,10 @@ if __name__ == "__main__":
         alpha = 0.04  # threshold of C
         dim_subspace = 12  # dimension of each subspace
         ro = 8  #
-    elif db == 'orl':
+    elif db == "orl":
         # load data
-        data = sio.loadmat('datasets/ORL_32x32.mat')
-        x, y = data['fea'].reshape((-1, 1, 32, 32)), data['gnd']
+        data = sio.loadmat("datasets/ORL_32x32.mat")
+        x, y = data["fea"].reshape((-1, 1, 32, 32)), data["gnd"]
         y = np.squeeze(y - 1)  # y in [0, 1, ..., K-1]
         # network and optimization parameters
         num_sample = x.shape[0]
@@ -260,10 +342,21 @@ if __name__ == "__main__":
 
     # load the pretrained weights which are provided by the original author in
     # https://github.com/panji1990/Deep-subspace-clustering-networks
-    ae_state_dict = torch.load('pretrained_weights_original/%s.pkl' % db)
+    ae_state_dict = torch.load("pretrained_weights_original/%s.pkl" % db)
     dscnet.ae.load_state_dict(ae_state_dict)
     print("Pretrained ae weights are loaded successfully.")
 
-    train(dscnet, x, y, epochs, weight_coef=weight_coef, weight_selfExp=weight_selfExp,
-          alpha=alpha, dim_subspace=dim_subspace, ro=ro, show=args.show_freq, device=device)
-    torch.save(dscnet.state_dict(), args.save_dir + '/%s-model.ckp' % args.db)
+    train(
+        dscnet,
+        x,
+        y,
+        epochs,
+        weight_coef=weight_coef,
+        weight_selfExp=weight_selfExp,
+        alpha=alpha,
+        dim_subspace=dim_subspace,
+        ro=ro,
+        show=args.show_freq,
+        device=device,
+    )
+    torch.save(dscnet.state_dict(), args.save_dir + "/%s-model.ckp" % args.db)
